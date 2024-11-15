6c53d92de9b545a07dfaf40fac724fa25cfb4f78
Pavić Jakov <0135280744@FPZ.HR>
2024-11-15 15:17:40 +0100

    treci zadatak

--- vj5-2.py
+++ vj5-2.py
@@ -10,4 +10,18 @@ if __name__ == "__main__":
         print(provjera_broja(uneseni_broj))
     except ValueError:
         print("Unesena vrijednost nije broj.")
-                
+
+def provjera broja(broj):
+if 10 <= 100:
+    return f"Broj {broj} je unutar raspona."
+else:
+    return f"Broj {broj} je izvan raspona."
+
+
+if _name_ == "__main__":
+    try:
+        uneseni_broj = int(input("Unesite broj: "))
+        print(provjera_broja(uneseni_broj))
+    except ValueError:
+        print("Unesena vrijednost nije broj.")
+
